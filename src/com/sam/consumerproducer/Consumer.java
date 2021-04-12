6e0d068237535b082c546059e2307129fd6569bd
suhail-tech <82331364+suhail-tech@users.noreply.github.com>
2021-04-12 11:12:57 +0530

    Add files via upload

---
+++ src/com/sam/consumerproducer/Consumer.java
@@ -0,0 +1,28 @@
+package com.sam.consumerproducer;
+
+import java.util.concurrent.BlockingQueue;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Consumer implements Runnable {
+
+	BlockingQueue<Integer> con;
+	int taken = -1;
+
+	public Consumer(BlockingQueue<Integer> con) {
+		this.con = con;
+	}
+
+	@Override
+	public void run() {
+		while (taken != 10) {
+			try {
+				taken = con.take();
+				System.out.println("Consumed By consumer : " + taken);
+			} catch (InterruptedException e) {
+			}
+		}
+
+	}
+
+}
---
+++ src/com/sam/consumerproducer/Main.java
@@ -0,0 +1,25 @@
+package com.sam.consumerproducer;
+	
+	
+	import java.util.concurrent.ArrayBlockingQueue;
+	import java.util.concurrent.BlockingQueue;
+	import java.util.concurrent.ExecutorService;
+	import java.util.concurrent.Executors;
+
+	public class Main {
+
+		public static void main(String[] args) {
+			BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+
+			Producer producer = new Producer(queue);
+			Consumer consumer = new Consumer(queue);
+
+			new Thread(producer).start();
+			ExecutorService executorService = Executors.newFixedThreadPool(2);
+			executorService.submit(consumer);
+			executorService.submit(consumer);
+
+			executorService.shutdown();
+		}
+
+	}
